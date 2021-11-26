6095263c07cafd71a0513cbc14a74f481ceda684
vnasypanyi_FPU <vnasypanyi@future-processing.com>
2021-11-26 13:59:39 +0200

    refactoring

--- Request.js
+++ Request.js
@@ -1,25 +1,34 @@
 var axios = require("axios").default;
+const fs = require('fs');
 
-var options = {
-    method: 'GET',
+
+/*var options = {
+    method: "GET",
     url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
     params: {s: 'Maximum Overdrive', r: 'json', y: '2009', page: '1'},
     headers: {
         'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com',
         'x-rapidapi-key': '0e8998a58bmsh1f691b3feebcb01p1b4eebjsn566ab1848fea'
     }
-};
+}
 
 
-axios.request(options).then(function (response) {
+
+
+
+
+/*axios.request(options).then(function (response) {
     const body1 = response.data['Search'][0];
     const body2 = body1['Title'];
-    console.log(response.request);
+    console.log(response.data);
 
    // const body1 = response.data;
     // const body2 = body1['Title'];
 }).catch(function (error) {
     console.error(error);
-});
+});*/
 
+let file1 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_models/GET_model_1.json')
+let options = JSON.parse(file1)
 
+console.log(options);
--- Request_test.js
+++ Request_test.js
@@ -1,40 +1,16 @@
 const request = require('request');
 const assert = require('chai').assert
 const assertArrays = require('chai-arrays');
-//const {options,body} = require("./variables");
+const fs = require("fs");
 var axios = require("axios").default;
-
-
-
-const options = {
-    method: 'GET',
-    url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
-    params: {s: 'Maximum Overdrive', r: 'json', y: '2009', page: '1'},
-    headers: {
-        'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com',
-        'x-rapidapi-key': '0e8998a58bmsh1f691b3feebcb01p1b4eebjsn566ab1848fea'
-    }
-};
-
-const options_noMovie = {
-    method: 'GET',
-    url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
-    params: {s: 'F8745628465', r: 'json', y: '2009', page: '1'},
-    headers: {
-        'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com',
-        'x-rapidapi-key': '0e8998a58bmsh1f691b3feebcb01p1b4eebjsn566ab1848fea'
-    }
-};
-
-const opt_res_restriction = {
-    method: 'GET',
-    url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
-    params: {s: 'F', r: 'json', y: '2009', page: '1'},
-    headers: {
-        'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com',
-        'x-rapidapi-key': '0e8998a58bmsh1f691b3feebcb01p1b4eebjsn566ab1848fea'
-    }
-};
+// const file1 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_templates/Get_template_1.json')
+// const options = JSON.parse(file1)
+// const file2 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_templates/Get_template_2.json')
+// const options_noMovie = JSON.parse(file2)
+// const file3 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_templates/Get_template_3.json')
+// const opt_res_restriction = JSON.parse(file3)
+// let file4 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_models/GET_model_1.json')
+// let test = JSON.parse(file4)
 
 const expStatusCode = 200;
 const expStatusMessage = 'OK'
@@ -52,53 +28,56 @@ const bodyExprespKeyFalse = "False";
 const bodyExpError_noMovie = "Movie not found!";
 const bodyExpError_TooManyRes = "Too many results.";
 
+const baseUrl = "https://movie-database-imdb-alternative.p.rapidapi.com/"
+const baseHeaders = {
+    "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+    "x-rapidapi-key": "0e8998a58bmsh1f691b3feebcb01p1b4eebjsn566ab1848fea"
+}
+const file1 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_templates/Get_template_1.json')
+const templBody = JSON.parse(file1)
+let config = {
+    method: 'get',
+    url: baseUrl,
+    headers: baseHeaders,
+    params: templBody
+}
 
 
 describe("GET request with valid data ", function() {
-    it("Expected: Correct status code and message",  async function () {
+    it("Validate response and json schema is ok",  async function () {
+        const response = await axios.request(config);
+        let file4 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_models/GET_model_1.json')
+        let expObject = JSON.parse(file4)
+        const expStatusMessage = 'OK'
+        const expStatusCode = 200;
 
-            let response = await axios.request(options);
+        const respCode = response.status;
+        const respMessage = response.statusText;
+        const body = response.data
 
-            const respCode = response.status;
-            const respMessage = response.statusText;
-            assert.equal(expStatusMessage, respMessage);
-            assert.equal(expStatusCode, respCode);
-    });
-
-    it("Expected: Correct body data structure and content",  async function () {
-
-            let response = await axios.request(options);
-
-            const body_title = response.data['Search'][0]['Title'];
-            const body_year = response.data['Search'][0]['Year'];
-            const body_imdbID = response.data['Search'][0]['imdbID'];
-            const body_type = response.data['Search'][0]['Type'];
-            const body_poster = response.data['Search'][0]['Poster'];
-            const total_results = response.data['totalResults'];
-            const body_respKey = response.data['Response'];
-            const bodyPropAmountCorrect = Object.keys(response.data).length;
-
-            assert.equal(body_title,bodyExp_title);
-            assert.equal(body_year,bodyExp_year);
-            assert.equal(body_imdbID,bodyExp_imdbID);
-            assert.equal(body_type,bodyExp_type);
-            assert.equal(body_poster,bodyExp_poster);
-            assert.equal(total_results,expTotalResults);
-            assert.equal(body_respKey,expResponceKey);
-            assert.equal(bodyPropAmountCorrect,expBodyPropAmount);
-
+        assert.deepEqual(body, expObject)
+        assert.equal(expStatusMessage, respMessage);
+        assert.equal(expStatusCode, respCode);
     });
 
     it("Expected: 'Movie not found!' if no movie exist + correct Status Code/Message",  async function () {
+        config['params']['s'] = "F8745628465"
+        const response = await axios.request(config)
+        let file4 = fs.readFileSync('C:/Users/vnasypanyi/IdeaProjects/IMDB_api/data_models/GET_model_1.json')
+        let test = JSON.parse(file4)
+        const expStatusMessage = 'OK'
+        const expStatusCode = 200;
+        const bodyExpPropAmFalse = 2;
+        const bodyExprespKeyFalse = "False";
 
-        let response = await axios.request(options_noMovie);
-
-
+        const body = response.data
         const bodyPropAmountFalse = Object.keys(response.data).length;
         const body_respKeyFalse = response.data['Response'];
         const body_Error_noMovie = response.data['Error'];
         const respCode = response.status;
         const respMessage = response.statusText;
+        const bodyExpError_noMovie = "Movie not found!";
+
         assert.equal(expStatusMessage, respMessage);
         assert.equal(expStatusCode, respCode);
         assert.equal(bodyExpPropAmFalse, bodyPropAmountFalse);
@@ -121,11 +100,5 @@ describe("GET request with valid data ", function() {
         assert.equal(bodyExprespKeyFalse, body_respKeyFalse);
         assert.equal(bodyExpError_TooManyRes, body_Error_TooManyRes);
     });
-
-
-
-
-
-
 
 });
